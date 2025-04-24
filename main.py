54df30d4e51899185ea6a4e1fd8f7591cd6beeb2
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2025-04-24 17:18:43 +0200

    12 commit

--- main.py
+++ main.py
@@ -8,96 +8,58 @@ from io import BytesIO
 import sys
 
 def unwrap2D(wrapped_image, quality_image=None) -> np.ndarray:
-    
-    class Pixel:
-        def __init__(self, value: float, reliability: float):
-            self.value = value
-            self.reliability = reliability
-            self.increment = 0
-            self.size = 1
-            self.head = self
-            self.last = self
-            self.next = None
+    def wrap(x): return x + np.where(x > 0.5, -1, np.where(x < -0.5, 1, 0))
 
-    class Edge:
-        def __init__(self, p1: Pixel, p2: Pixel):
-            self.p1 = p1
-            self.p2 = p2
-            self.reliability = p1.reliability + p2.reliability
-            diff = p1.value - p2.value
-            self.increment = -1 if diff > 0.5 else (1 if diff < -0.5 else 0)
-
-    def wrap(pixel_value):
-        return pixel_value +  np.where(pixel_value > 0.5, -1, np.where(pixel_value < -0.5, 1, 0))
-    
-    def miguel_quality(wrapped):
-            
-        WIP = wrapped[1:-1, 1:-1]
-        
-        H  = wrap(wrapped[1:-1, :-2] - WIP) - wrap(WIP - wrapped[1:-1, 2:])
-        V  = wrap(wrapped[:-2, 1:-1] - WIP) - wrap(WIP - wrapped[2:, 1:-1])
-        D1 = wrap(wrapped[:-2, :-2] - WIP)  - wrap(WIP - wrapped[2:, 2:])
-        D2 = wrap(wrapped[:-2, 2:] - WIP)   - wrap(WIP - wrapped[2:, :-2])
-    
-        reliability = 1.0/(H**2 + V**2 + D1**2 + D2**2)
-    
-        miguel_array=np.zeros_like(wrapped)
-        miguel_array[1:-1, 1:-1] = np.sqrt(reliability)
-        return miguel_array
+    def compute_quality(wrapped):
+        W = wrapped[1:-1, 1:-1]
+        diffs = [
+            wrap(wrapped[1:-1, :-2] - W) - wrap(W - wrapped[1:-1, 2:]),
+            wrap(wrapped[:-2, 1:-1] - W) - wrap(W - wrapped[2:, 1:-1]),
+            wrap(wrapped[:-2, :-2] - W) - wrap(W - wrapped[2:, 2:]),
+            wrap(wrapped[:-2, 2:] - W) - wrap(W - wrapped[2:, :-2])
+        ]
+        reliability = 1.0 / sum(d**2 for d in diffs)
+        result = np.zeros_like(wrapped)
+        result[1:-1, 1:-1] = np.sqrt(reliability)
+        return result
 
     if quality_image is None:
-        quality_image = miguel_quality(wrapped_image)
-        
+        quality_image = compute_quality(wrapped_image)
     if wrapped_image.shape != quality_image.shape:
         raise ValueError("Input arrays must have the same dimensions")
-    
-    height, width = wrapped_image.shape
-    
-    # Flatten images into list of Pixel objects
-    pixels = np.array([
-        Pixel(wrapped_image.flat[i], quality_image.flat[i]) for i in range(wrapped_image.size)
-    ])
 
-    # Build edges between horizontal and vertical neighbors
-    edges = []
-    for r in range(height):
-        for c in range(width):
-            idx = r * width + c
-            if c < width - 1:
-                edges.append(Edge(pixels[idx], pixels[idx + 1]))
-            if r < height - 1:
-                edges.append(Edge(pixels[idx], pixels[idx + width]))
+    h, w = wrapped_image.shape
+    size = wrapped_image.size
 
-    # Sort edges by reliability in descending order
-    edges.sort(key=lambda e: e.reliability, reverse=True)
+    class P:
+        def __init__(self, v, r):
+            self.v, self.r, self.i, self.s, self.h, self.l, self.n = v, r, 0, 1, self, self, None
 
-    def merge_groups(p1: Pixel, p2: Pixel, edge_increment: int):
-        group1 = p1.head
-        group2 = p2.head
+    pixels = np.array([P(wrapped_image.flat[i], quality_image.flat[i]) for i in range(size)])
 
-        if group1.size < group2.size:
-            group1, group2 = group2, group1
-            p1, p2 = p2, p1
-            edge_increment = -edge_increment
+    class E:
+        def __init__(self, p1, p2):
+            d = p1.v - p2.v
+            self.p1, self.p2 = p1, p2
+            self.r = p1.r + p2.r
+            self.i = -1 if d > 0.5 else (1 if d < -0.5 else 0)
 
-        # Merge group2 into group1
-        group1.last.next = group2
-        group1.last = group2.last
-        group1.size += group2.size
+    edges = [E(pixels[r*w+c], pixels[r*w+c+1]) for r in range(h) for c in range(w-1)] + \
+            [E(pixels[r*w+c], pixels[(r+1)*w+c]) for r in range(h-1) for c in range(w)]
+    edges.sort(key=lambda e: e.r, reverse=True)
 
-        delta = p1.increment - edge_increment - p2.increment
-        while group2:
-            group2.head = group1
-            group2.increment += delta
-            group2 = group2.next
+    def merge(p1, p2, inc):
+        g1, g2 = p1.h, p2.h
+        if g1.s < g2.s: g1, g2, p1, p2, inc = g2, g1, p2, p1, -inc
+        g1.l.n, g1.l, g1.s = g2, g2.l, g1.s + g2.s
+        delta = p1.i - inc - p2.i
+        while g2: g2.h, g2.i, g2 = g1, g2.i + delta, g2.n
 
-    # Process edges to unwrap phase
-    for edge in edges:
-        if edge.p1.head != edge.p2.head:
-            merge_groups(edge.p1, edge.p2, edge.increment)
+    for e in edges:
+        if e.p1.h != e.p2.h:
+            merge(e.p1, e.p2, e.i)
 
-    unwrapped = np.array([p.value + p.increment for p in pixels]).reshape((height, width))
-    return unwrapped
+    return np.array([p.v + p.i for p in pixels]).reshape((h, w))
 
 def scale_array(arr, new_shape):
     old_rows, old_cols = arr.shape
